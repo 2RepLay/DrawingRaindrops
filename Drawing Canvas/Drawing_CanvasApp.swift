2ef55fcc7c6e21884d4f0f7f8c0da63503cf6c92
Nikita Yankov <evne.yankovnikita@gmail.com>
2023-03-04 22:43:53 +0200

    Initial Commit

---
+++ Drawing Canvas/Drawing_CanvasApp.swift
@@ -0,0 +1,17 @@
+//
+//  Drawing_CanvasApp.swift
+//  Drawing Canvas
+//
+//  Created by nikita on 04.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct Drawing_CanvasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
